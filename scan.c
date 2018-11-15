57bceadd160dc3950844794a01993a4625027428
Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>
2018-11-15 00:40:30 -0600

    Parses file correctly.  Debug adornments active.
    Signed-off-by: Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>

--- scan.c
+++ scan.c
@@ -11,30 +11,42 @@ int main()
 		c = getchar();
 	};
 
+	void skip_white() {
+		while(c != EOF && c == ' ') c = getchar();
+	};
+
         void scan_string() {
 		while(c != EOF && c != '"') c = getchar();
 		c = getchar();
-		int aux_c = *"<";
 		putchar(*"<");
-		while(c != EOF && c != '"') c = getchar();
+		while(c != EOF && c != '"') {putchar(c); c = getchar();}
 		c = getchar();
 		putchar(*">");
         };
 
         void scan_num() {
 		while(c != EOF && (c < '0' || '9' < c)) c = getchar();
-		putchar(*"<");
-		while(c != EOF && (c < '0' && '9' < c || c=='.')) c = getchar();
-		putchar(*">");
+		putchar(*"N");
+		while(c != EOF && (('0' <= c && c <= '9') || c=='.')) {putchar(c); c = getchar();}
+		putchar(*"N");
         };
 
 	c = getchar();
 
 	consume("{");
-	while(c != EOF)
+	while(c != EOF && c != '}')
 		{
+		scan_string();
 		putchar(c);
 		c = getchar();
+		skip_white();
+		putchar(*"!"); putchar(c);
+		if('0' <= c && c <= '9') {
+			scan_num();
+		} else if(c == '"') {
+			scan_string();
+		}
+		putchar(*"\n");
 		}
 
 	return 0;
