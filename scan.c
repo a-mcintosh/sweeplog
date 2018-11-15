fbf07c6cc1f9892bdcfb678a9cc4aeee8392e15f
Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>
2018-11-15 06:22:51 -0600

    Produce two output files, pro-forma correct, need to test for correct SQL details.
    Signed-off-by: Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>

--- scan.c
+++ scan.c
@@ -5,6 +5,8 @@
 int main()
 {
 	int c;
+	FILE *fields = fopen("tmp-scan-fields.txt", "w");
+	FILE *values = fopen("tmp-scan-values.txt", "w");
         
 	void consume(char *ch) {
 		while(c != EOF && c != (short)*ch) c = getchar();
@@ -15,47 +17,64 @@ int main()
 		while(c != EOF && c == ' ') c = getchar();
 	};
 
-        void scan_string() {
+        void scan_string(FILE *of) {
 		while(c != EOF && c != '"') c = getchar();
 		c = getchar();
-		putchar(*"\"");
-		while(c != EOF && c != '"') {putchar(c); c = getchar();}
+		putc(*"\"", of);
+		while(c != EOF && c != '"') {putc(c, of); c = getchar();}
 		c = getchar();
-		putchar(*"\"");
+		putc(*"\"", of);
         };
 
-        void scan_num() {
+        void scan_num(FILE *of) {
 		while(c != EOF && (c < '0' || '9' < c)) c = getchar();
-		while(c != EOF && (('0' <= c && c <= '9') || c=='.')) {putchar(c); c = getchar();}
+		while(c != EOF && (('0' <= c && c <= '9') || c=='.')) {putc(c, of); c = getchar();}
         };
 
-        void scan_token() {
+        void scan_token(FILE *of) {
 		while(c != EOF && (('0' <= c && c <= '9') || ('a' <= c && c <= 'z') || c=='.')) {
-			putchar(c); c = getchar();
+			putc(c, of); c = getchar();
 		}
         };
+
+	void init_fields() {
+		fprintf(fields, "INSERT INTO test_table (");
+	};
+
+	void finish_fields() {
+		fprintf(fields, ")\n");
+		fclose(fields);
+	};
+
+	void finish_values() {
+		fprintf(values, ");\n");
+		fclose(values);
+	};
+
+	void init_values() {
+		fprintf(values, "VALUES (");
+	};
 
 	c = getchar();
-
-	consume("{"); putchar(*"{"); putchar(*"\n");
+	init_fields();
+	init_values();
+	consume("{");
 	while(c != EOF && c != '}')
 		{
-		scan_string();
-		putchar(c);
+		scan_string(fields);
 		c = getchar();
 		skip_white();
-		putchar(*"\t");
 		if('0' <= c && c <= '9') {
-			scan_num();
+			scan_num(values);
 		} else if(c == '"') {
-			scan_string();
+			scan_string(values);
 		} else if(c == 't' || c == 'f') {
-			scan_token();
+			scan_token(values);
 		}
-		if (c != '}') putchar(*",");
-		putchar(*"\n");
+		if (c != '}') {putc(',', fields); putc(',', values);}
 		}
-	putchar(*"}"); putchar(*"\n");
+	finish_fields();
+	finish_values();
 
 	return 0;
 }
