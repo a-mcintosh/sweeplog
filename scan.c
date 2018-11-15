106a56a5e49ad563bfbc789b62c5b4930dc9d958
Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>
2018-11-14 23:49:30 -0600

    Make system added.
    Signed-off-by: Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>

--- scan.c
+++ scan.c
@@ -2,12 +2,36 @@
 
 /* copy input to output */
 
-main()
+int main()
 {
 	int c;
+        
+	void consume(int *ch) {
+		while(c != EOF && c != ord(ch)) c = getchar();
+		c = getchar();
+	};
+
+        void scan_string() {
+		while(c != EOF && c != '"') c = getchar();
+		c = getchar();
+		int aux_c = *"<";
+		putchar(*"<");
+		while(c != EOF && c != '"') c = getchar();
+		c = getchar();
+		putchar(ord(">"));
+        };
+
+        void scan_num() {
+		while(c != EOF && (c < '0' || '9' < c)) c = getchar();
+		putchar(ord("<"));
+		while(c != EOF && (c < '0' && '9' < c || c=='.')) c = getchar();
+		putchar(ord(">"));
+        };
 
 	c = getchar();
 
+	while(c != EOF && c != '{') c = getchar();
+	c = getchar();
 	while(c != EOF)
 		{
 		putchar(c);
